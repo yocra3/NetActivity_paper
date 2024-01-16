a0440a576db784c782d105bcc37515a3f1e03ef0
Carlos Ruiz <yocra3@gmail.com>
2024-01-16 11:23:28 +0100

    Remove results not included in the manuscript for GSAS validation

--- scripts/3.EvaluateModel/6.validateScores.R
+++ scripts/3.EvaluateModel/6.validateScores.R
@@ -30,13 +30,6 @@ bad_phenos <- c(bad_phenos, colnames(phenos)[grep("RNAseq", colnames(phenos))],
 
 phenos <- phenos[, !colnames(phenos) %in% bad_phenos]
 
-
-cor_doub <- cor(t(assay(gse179848_scores)), data.matrix(phenos[, "Doubling_rate_Udays_per_division"]), use = "pairwise.complete.obs")
-
-doup_tib <- tibble(cors = cor_doub, GeneSet = rownames(gse179848_scores), 
-    GeneSetName = rowData(gse179848_scores)$Term)
-arrange(doup_tib, desc(abs(cors))) %>% data.frame() %>% head(20)
-
 doub_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:1902969", ])),
             GSAS2 = as.vector(assay(gse179848_scores["GO:0060623", ])),
             Division_Rate = gse179848_scores$Doubling_rate_Udays_per_division, 
@@ -48,6 +41,7 @@ doub_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:1902969", ]))
 cor.test(doub_df$GSAS1, doub_df$Division_Rate)
 ## r = 0.56 p-value < 2.2e-16
 
+## Figure 1G
 p1 <- ggplot(doub_df, aes(x = GSAS1, y = Division_Rate)) +
     geom_point() +
     geom_smooth(method = "lm") +
@@ -61,17 +55,8 @@ dev.off()
 
 
 ## Glycolisis
-glyc <- colnames(pheno_cors)[grep("gly", colnames(pheno_cors))]
-pheno_cors[glyc, glyc]
-
-cor_glyc <- cor(t(assay(gse179848_scores)), data.matrix(phenos[, "ATPglyc_UpmolATP_per_min_per_20kcells"]), use = "complete.obs")
-metab_glyc <- tibble(cors = cor_glyc[, 1], GeneSet = rownames(gse179848_scores), 
-    GeneSetName = rowData(gse179848_scores)$Term)
-arrange(metab_glyc, desc(abs(cors))) %>% data.frame() %>% head(10)
-
 glyco_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:0051156", ])),
             GSAS2 = as.vector(assay(gse179848_scores["GO:0006007", ])),
-            GSAS3 =  as.vector(assay(gse179848_scores["path:hsa00020", ])),
             Glycolisis = gse179848_scores$ATPglyc_UpmolATP_per_min_per_20kcells, 
             TotalATP = gse179848_scores$ATPtotal_UpmolATP_per_min_per_20kcells, 
             Treatment = factor(gse179848_scores$Treatments),
@@ -85,7 +70,6 @@ glyco_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:0051156", ])
                 State = ifelse(Cell_type == "Mutated", "OXPHOS mutation", State),
                 GlycProp = Glycolisis/TotalATP)
 
-glyco_df$logGlyc <- log(glyco_df$Glycolisis)
 
 states <- unique(glyco_df$State)
 names(states) <- states
@@ -107,22 +91,11 @@ lapply(states, function(state){
 })
 
 
-lapply(states, function(state){
-    summary(lm(GSAS1 ~ logGlyc, glyco_df,  subset = State == state))
-})
-lapply(states, function(state){
-    sub <- subset(glyco_df, State == state)
-    cor.test(sub$GSAS1, sub$logGlyc)
-})
-
-
 cor.test(glyco_df$GSAS1, glyco_df$Glycolisis)
 cor.test(glyco_df$GSAS2, glyco_df$Glycolisis)
-cor.test(glyco_df$GSAS3, glyco_df$Glycolisis)
-
-plot(glyco_df$GSAS2, log(glyco_df$Glycolisis))
 
 
+## Figure 1H
 p3 <- glyco_df %>%
     filter(State != "Other") %>%
 ggplot(aes(x = GSAS1, y = Glycolisis)) +
@@ -137,6 +110,7 @@ png("figures/validation_score_glycolisis.png", height = 1500, width = 2100, res 
 p3
 dev.off()
 
+## Sup Figure 15
 p4 <- glyco_df %>%
     ggplot(aes(x = GSAS2, y = Glycolisis)) +
         geom_point(aes(color = State)) +
@@ -167,161 +141,4 @@ summary(lm(Glycolisis ~ GSAS1 + Treatment*Cell_type, glyco_df))
 
 summary(lm(Glycolisis ~ Treatment + Cell, glyco_df, subset = Cell_type == "Control"))
 
-
-
-## Oxidative phosphorylation
-ox_df <- data.frame(GSAS = as.vector(assay(gse179848_scores["GO:0002082", ])),
-            GSAS2 = as.vector(assay(gse179848_scores["path:hsa00020", ])),
-            oxPhos = gse179848_scores$ATPox_UpmolATP_per_min_per_20kcells, 
-            TotalATP = gse179848_scores$ATPtotal_UpmolATP_per_min_per_20kcells, 
-            Treatment = factor(gse179848_scores$Treatments),
-            Cell = gse179848_scores$Cell_line)  %>%
-            filter(!is.na(oxPhos)) %>%
-            mutate(Cell_type = ifelse(Cell %in% paste0("HC", 1:4), "Control", "Mutated"),
-                Treatment = fct_relevel(Treatment, "Control"),
-                State = ifelse(Treatment == "Control", "Basal", 
-                    ifelse(grepl("^Oligo|mito", Treatment), "OXPHOS inhibition", 
-                        ifelse(Treatment %in% c("2DG", "Galactose"), "Glycolisis inhibition", "Other"))),
-                State = ifelse(Cell_type == "Mutated", "OXPHOS mutation", State),
-                OxProp = oxPhos/TotalATP)
-
-ox_df %>%
-    ggplot(aes(x = GSAS, y = oxPhos)) +
-    geom_point() +
-        geom_smooth(method = "lm") +
-        xlab("OxPhos GSAS") +
-        ylab("OxPhos") +
-        facet_grid(~ State) +
-        theme_bw() 
-ox_df %>%
-    ggplot(aes(x = GSAS2, y = oxPhos)) +
-    geom_point() +
-        geom_smooth(method = "lm") +
-        xlab("OxPhos GSAS") +
-        ylab("OxPhos") +
-        facet_grid(~ State) +
-        theme_bw() 
-
-states <- unique(ox_df$State)
-names(states) <- states
-lapply(states, function(state){
-    summary(lm(GSAS ~ oxPhos, ox_df,  subset = State == state))
-})
-lapply(states, function(state){
-    sub <- subset(ox_df, State == state)
-    cor.test(sub$GSAS, sub$oxPhos)
-})
-
-sub <- subset(ox_df, State %in% c("Basal", "Other", "Glycolisis inhibition"))
-cor.test(sub$GSAS2, sub$oxPhos)
-
-### GSVA
-library(GSVA)
-library(parallel)
-library(org.Hs.eg.db)
-paths.vec <- rownames(gse179848_scores)
-path.map <- read.table("results/GTEx_coding/go_kegg_filt2_gene_map.tsv", header = TRUE)
-path_genes <- mclapply(paths.vec, function(x) subset(path.map, PathwayID == x & !is.na(Symbol))$Symbol, mc.cores = 10)
-names(path_genes) <- paths.vec
-
-load("results/GSE179848/rawCounts_GSE179848.Rdata")
-rownames(gse179848) <- mapIds(org.Hs.eg.db,
-    keys = rownames(gse179848),
-    column = 'ENSEMBL',
-    keytype = 'REFSEQ')
-
-gse179848 <- gse179848[!is.na(rownames(gse179848)), ]
-gse179848 <- gse179848[!duplicated(rownames(gse179848)), ]
-names(assays(gse179848)) <- "count"
-gsva_res <- gsva(gse179848, path_genes,  kcdf = "Poisson")
-save(gsva_res, file = "results/GSE179848/gsva_GSE179848.Rdata")
-
-doub_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:1902969", ])),
-            GSAS2 = as.vector(assay(gse179848_scores["GO:0060623", ])),
-            GSAS1g = as.vector(assay(gsva_res["GO:1902969", ])),
-            Division_Rate = gse179848_scores$Doubling_rate_Udays_per_division, 
-            Treatment = gse179848_scores$Treatments,
-            Cell = gse179848_scores$Cell_line)  %>%
-            filter(!is.na(Division_Rate)) %>%
-            filter(!is.na(Cell)) %>%
-            mutate(Cell_type = ifelse(Cell %in% paste0("HC", 1:4), "Control", "Mutated"))
-cor.test(doub_df$GSAS1, doub_df$Division_Rate)
-cor.test(doub_df$GSAS1g, doub_df$Division_Rate)
-
-
-
-glyco_df <- data.frame(GSAS1 = as.vector(assay(gse179848_scores["GO:0051156", ])),
-            GSAS2 = as.vector(assay(gse179848_scores["GO:0006007", ])),
-            GSAS1g = as.vector(assay(gsva_res["GO:0051156", ])),
-            GSAS2g = as.vector(assay(gsva_res["GO:0006007", ])),
-            Glycolisis = gse179848_scores$ATPglyc_UpmolATP_per_min_per_20kcells, 
-            TotalATP = gse179848_scores$ATPtotal_UpmolATP_per_min_per_20kcells, 
-            Treatment = factor(gse179848_scores$Treatments),
-            Cell = gse179848_scores$Cell_line)  %>%
-            filter(!is.na(Glycolisis)) %>%
-            mutate(Cell_type = ifelse(Cell %in% paste0("HC", 1:4), "Control", "Mutated"),
-                Treatment = fct_relevel(Treatment, "Control"),
-                State = ifelse(Treatment == "Control", "Basal", 
-                    ifelse(grepl("^Oligo|mito", Treatment), "OXPHOS inhibition", 
-                        ifelse(Treatment %in% c("2DG", "Galactose"), "Glycolisis inhibition", "Other"))),
-                State = ifelse(Cell_type == "Mutated", "OXPHOS mutation", State),
-                GlycProp = Glycolisis/TotalATP)
-cor.test(glyco_df$GSAS2, glyco_df$Glycolisis)
-cor.test(glyco_df$GSAS2g, glyco_df$Glycolisis)
-
-cor.test(glyco_df$GSAS1, glyco_df$Glycolisis)
-cor.test(glyco_df$GSAS1g, glyco_df$Glycolisis)
-
-
-ox_df <- data.frame(GSAS = as.vector(assay(gse179848_scores["GO:0002082", ])),
-            GSASg = as.vector(assay(gsva_res["GO:0002082", ])),
-            oxPhos = gse179848_scores$ATPox_UpmolATP_per_min_per_20kcells, 
-            TotalATP = gse179848_scores$ATPtotal_UpmolATP_per_min_per_20kcells, 
-            Treatment = factor(gse179848_scores$Treatments),
-            Cell = gse179848_scores$Cell_line)  %>%
-            filter(!is.na(oxPhos)) %>%
-            filter(oxPhos < 500) %>%
-            mutate(Cell_type = ifelse(Cell %in% paste0("HC", 1:4), "Control", "Mutated"),
-                Treatment = fct_relevel(Treatment, "Control"),
-                State = ifelse(Treatment == "Control", "Basal", 
-                    ifelse(grepl("^Oligo|mito", Treatment), "OXPHOS inhibition", 
-                        ifelse(Treatment %in% c("2DG", "Galactose"), "Glycolisis inhibition", "Other"))),
-                State = ifelse(Cell_type == "Mutated", "OXPHOS mutation", State),
-                OxProp = oxPhos/TotalATP)
-
-
-cor.test(ox_df$GSAS, ox_df$oxPhos)
-cor.test(ox_df$GSASg, ox_df$oxPhos)
-
-sub <- subset(ox_df, State %in% c("Basal", "Other", "Glycolisis inhibition"))
-cor.test(sub$GSAS, sub$oxPhos)
-cor.test(sub$GSASg, sub$oxPhos)
-
-
-ox_df %>%
-    ggplot(aes(x = GSAS, y = oxPhos)) +
-    geom_point() +
-        geom_smooth(method = "lm") +
-        xlab("OxPhos GSAS") +
-        ylab("OxPhos") +
-        facet_grid(~ State) +
-        theme_bw() 
-
-
-ox_df %>%
-    ggplot(aes(x = GSASg, y = oxPhos)) +
-    geom_point() +
-        geom_smooth(method = "lm") +
-        xlab("OxPhos GSVA") +
-        ylab("OxPhos") +
-        facet_grid(~ State) +
-        theme_bw() 
-
-par(mfrow = c(1, 2))
-plot(ox_df$GSAS, ox_df$oxPhos, main = "NetActivity")
-plot(ox_df$GSASg, ox_df$oxPhos, main = "GSVA")
-
-## hipathia
-hip_pathways <- load_pathways(species = "hsa")
-translated_names <- get_path_names(hip_pathways, names(hip_pathways$path.norm))
 
